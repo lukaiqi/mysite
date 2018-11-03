f8bf134771f8f43cd0965f0c039cd6aeb3c7c894
lukaiqi <1263041461@qq.com>
2018-11-03 11:23:45 +0800

    修改首页内容

--- mysite/views.py
+++ mysite/views.py
@@ -4,15 +4,28 @@ from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
 from django.db.models import Sum
 from django.core.cache import cache
-from read_statistics.utils import get_seven_days_read_data, get_today_hot_data, get_yesterday_hot_data
-from blog.models import Blog
+from django.db.models import Count
+from read_statistics.utils import get_seven_days_read_data, \
+    get_today_hot_data, get_thirty_days_read_data
+from blog.models import Blog,BlogType
 from user.models import SendMail
 from user.views import get_ip
-
+from blog.views import get_blog_list_common_data
 
 def get_7_days_hot_blogs():
     today = timezone.now().date()
     date = today - datetime.timedelta(days=7)
+    blogs = Blog.objects \
+        .filter(read_details__date__lt=today, read_details__date__gte=date) \
+        .values('id', 'title') \
+        .annotate(read_num_sum=Sum('read_details__read_num')) \
+        .order_by('-read_num_sum')
+    return blogs[:7]
+
+
+def get_30_days_hot_blogs():
+    today = timezone.now().date()
+    date = today - datetime.timedelta(days=30)
     blogs = Blog.objects \
         .filter(read_details__date__lt=today, read_details__date__gte=date) \
         .values('id', 'title') \
@@ -27,14 +40,21 @@ def home(request):
 
     # 获取7天热门博客的缓存数据
     hot_blogs_for_7_days = cache.get('hot_blogs_for_7_days')
+    hot_blogs_for_30_days = cache.get('hot_blogs_for_30_days')
     if hot_blogs_for_7_days is None:
         hot_blogs_for_7_days = get_7_days_hot_blogs()
         cache.set('hot_blogs_for_7_days', hot_blogs_for_7_days, 3600)
 
+    if hot_blogs_for_30_days is None:
+        hot_blogs_for_30_days = get_30_days_hot_blogs()
+        cache.set('hot_blogs_for_30_days', hot_blogs_for_30_days, 3600)
+
     context = {}
     context['dates'] = dates
     context['read_nums'] = read_nums
+    # context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog'))
+    context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog'))
     context['today_hot_data'] = get_today_hot_data(blog_content_type)
-    context['yesterday_hot_data'] = get_yesterday_hot_data(blog_content_type)
     context['hot_blogs_for_7_days'] = hot_blogs_for_7_days
+    context['hot_blogs_for_30_days'] = hot_blogs_for_30_days
     return render(request, 'home.html', context)
--- read_statistics/utils.py
+++ read_statistics/utils.py
@@ -22,6 +22,20 @@ def read_statistics_once_read(request, obj):
         readDetail.save()
     return key
 
+
+def get_thirty_days_read_data(content_type):
+    today = timezone.now().date()
+    dates = []
+    read_nums = []
+    for i in range(30, 0, -1):
+        date = today - datetime.timedelta(days=i)
+        dates.append(date.strftime('%m/%d'))
+        read_details = ReadDetail.objects.filter(content_type=content_type, date=date)
+        result = read_details.aggregate(read_num_sum=Sum('read_num'))
+        read_nums.append(result['read_num_sum'] or 0)
+    return dates, read_nums
+
+
 def get_seven_days_read_data(content_type):
     today = timezone.now().date()
     dates = []
@@ -34,13 +48,8 @@ def get_seven_days_read_data(content_type):
         read_nums.append(result['read_num_sum'] or 0)
     return dates, read_nums
 
+
 def get_today_hot_data(content_type):
     today = timezone.now().date()
     read_details = ReadDetail.objects.filter(content_type=content_type, date=today).order_by('-read_num')
-    return read_details[:7]
-
-def get_yesterday_hot_data(content_type):
-    today = timezone.now().date()
-    yesterday = today - datetime.timedelta(days=1)
-    read_details = ReadDetail.objects.filter(content_type=content_type, date=yesterday).order_by('-read_num')
     return read_details[:7]
