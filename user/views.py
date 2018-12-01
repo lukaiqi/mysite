58263699b7569ed0dc45a1329917089870a93d30
lukaiqi <1263041461@qq.com>
2018-12-01 12:07:46 +0800

    修改访问权限

--- user/views.py
+++ user/views.py
@@ -287,15 +287,18 @@ def forgot_password(request):
 
 
 def upload_file(request):
-    if request.method == "POST":  # 请求方法为POST时，进行处理
-        myFile = request.FILES.get("file", None)  # 获取上传的文件，如果没有文件，则默认为None
-        if not myFile:
-            return HttpResponse("未选择上传文件")
-        destination = open(os.path.join("/home/mysite/files", myFile.name), 'wb+')  # 打开特定的文件进行二进制的写操作
-        for chunk in myFile.chunks():  # 分块写入文件
-            destination.write(chunk)
-        destination.close()
-        return redirect('/user/file_list')
+    if request.user.is_superuser:
+        if request.method == "POST":  # 请求方法为POST时，进行处理
+            myFile = request.FILES.get("file", None)  # 获取上传的文件，如果没有文件，则默认为None
+            if not myFile:
+                return HttpResponse("未选择上传文件")
+            destination = open(os.path.join("/home/mysite/files", myFile.name), 'wb+')  # 打开特定的文件进行二进制的写操作
+            for chunk in myFile.chunks():  # 分块写入文件
+                destination.write(chunk)
+            destination.close()
+            return redirect('/user/file_list')
+    else:
+        return render(request, 'error.html')
 
 
 def upload(request):
