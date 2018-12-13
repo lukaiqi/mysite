00b47f9f4e4eb407c48c3cc1468d25eacf851b9e
lukaiqi <1263041461@qq.com>
2018-12-13 22:57:10 +0800

    删除首页内容，添加live2d和图灵机器人

--- mysite/views.py
+++ mysite/views.py
@@ -1,60 +1,33 @@
-import datetime
+import requests
 from django.shortcuts import render
-from django.contrib.contenttypes.models import ContentType
-from django.utils import timezone
-from django.db.models import Sum
-from django.core.cache import cache
-from django.db.models import Count
-from read_statistics.utils import get_seven_days_read_data, \
-    get_yesterday_hot_data, get_today_hot_data
-from blog.models import Blog, BlogType
-from info.views import get_notice
-
-
-def get_7_days_hot_blogs():
-    today = timezone.now().date()
-    date = today - datetime.timedelta(days=7)
-    blogs = Blog.objects \
-        .filter(read_details__date__lt=today, read_details__date__gte=date) \
-        .values('id', 'title') \
-        .annotate(read_num_sum=Sum('read_details__read_num')) \
-        .order_by('-read_num_sum')
-    return blogs[:10]
-
-
-def get_30_days_hot_blogs():
-    today = timezone.now().date()
-    date = today - datetime.timedelta(days=30)
-    blogs = Blog.objects \
-        .filter(read_details__date__lt=today, read_details__date__gte=date) \
-        .values('id', 'title') \
-        .annotate(read_num_sum=Sum('read_details__read_num')) \
-        .order_by('-read_num_sum')
-    return blogs[:10]
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
-    blog_content_type = ContentType.objects.get_for_model(Blog)
-    dates, read_nums = get_seven_days_read_data(blog_content_type)
+    return render(request, 'home.html')
 
-    # 获取7天热门博客的缓存数据
-    hot_blogs_for_7_days = cache.get('hot_blogs_for_7_days')
-    hot_blogs_for_30_days = cache.get('hot_blogs_for_30_days')
-    if hot_blogs_for_7_days is None:
-        hot_blogs_for_7_days = get_7_days_hot_blogs()
-        cache.set('hot_blogs_for_7_days', hot_blogs_for_7_days, 3600)
 
-    if hot_blogs_for_30_days is None:
-        hot_blogs_for_30_days = get_30_days_hot_blogs()
-        cache.set('hot_blogs_for_30_days', hot_blogs_for_30_days, 3600)
-
-    context = {}
-    context['dates'] = dates
-    context['notice'] = get_notice()
-    context['read_nums'] = read_nums
-    context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog'))
-    context['today_hot_data'] = get_today_hot_data(blog_content_type)
-    context['yesterday_hot_data'] = get_yesterday_hot_data(blog_content_type)
-    context['hot_blogs_for_7_days'] = hot_blogs_for_7_days
-    context['hot_blogs_for_30_days'] = hot_blogs_for_30_days
-    return render(request, 'home.html', context)
+@csrf_exempt
+def tuling(request):
+    name = request.POST.get('userId')
+    text = request.POST.get('text')
+    url = "http://openapi.tuling123.com/openapi/api/v2"
+    # url = "http://examle.com"
+    payload = "{" \
+              "\"perception\": {" \
+              "\"inputText\": {" \
+              "\"text\":" + repr(text) + \
+              "}," \
+              "}," \
+              "\"userInfo\": {" \
+              "\"apiKey\": \"67fd8719169c41d6805c83e267edba24\"," \
+              "\"userId\":" + repr(name) + \
+              "}" \
+              "}"
+    headers = {
+        'content-type': "application/json",
+        'charset': "utf-8",
+    }
+    response = requests.request("POST", url, data=payload.encode(), headers=headers)
+    return HttpResponse(response.text)
